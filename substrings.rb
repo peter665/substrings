47d3863df4adde4c8b86902cf2e048bd6589938f
Peter <peto.mester@gmail.com>
2017-01-14 13:46:47 +0100

    Solved

---
+++ substrings.rb
@@ -0,0 +1,14 @@
+
+
+def substrings str, dictionary
+  res = Hash.new(0)
+  str_arr = str.split(" ")
+  dictionary.each do |el|
+    for i in 0...str_arr.length do
+      if str_arr[i].include? el
+        res[el] += 1
+      end
+    end
+  end
+  res
+end
